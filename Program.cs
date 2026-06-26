e721c4972b607ba01f44c9f3b2ca8651c3f51bff
Scott Golby <scott@golby.com>
2026-06-26 19:29:38 -0400

    v1.0.0 of the Show Desktop util

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+[DllImport("user32.dll")]
+static extern void keybd_event(byte bVk, byte bScan, uint dwFlags, nuint dwExtraInfo);
+
+const byte VK_LWIN = 0x5B;
+const uint KEYEVENTF_KEYUP = 0x0002;
+
+keybd_event(VK_LWIN, 0, 0, 0);
+keybd_event((byte)'D', 0, 0, 0);
+keybd_event((byte)'D', 0, KEYEVENTF_KEYUP, 0);
+keybd_event(VK_LWIN, 0, KEYEVENTF_KEYUP, 0);
+
+Environment.Exit(0);
